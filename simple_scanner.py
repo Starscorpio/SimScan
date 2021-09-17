4961a349b7e7df1a2dc7a81c209cfb814513342f
Vedant Tare <41759419+Starscorpio@users.noreply.github.com>
2021-09-17 20:47:33 +0530

    Update simple_scanner.py

--- simple_scanner.py
+++ simple_scanner.py
@@ -2,14 +2,14 @@ import sys
 import socket
 from datetime import datetime
 
-#Defining our target
+#Defining target
 if len(sys.argv) == 2:
-	target = socket.gethostbyname(sys.argv[1]) #translating hostname to IPv4
+	target = socket.gethostbyname(sys.argv[1]) #converting hostname to IP address
 else:
 		print("Not enough or too much arguments")
 		print("Use: python3 %s <ip>")
 
-#Adding a banner
+#Adding a banner and name
 print("-" * 50)
 print("Scanning started for host: " + target)
 print("Time of scan: "+ str(datetime.now()))
@@ -20,16 +20,16 @@ try:
 		for port in range(1,65535):
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			socket.setdefaulttimeout(1)
-			result = s.connect_ex((target, port)) #returns an error indicator
+			result = s.connect_ex((target, port)) #returns error if any
 			try:
-				service = socket.getservbyport(port, 'tcp')
+				service = socket.getservbyport(port, 'tcp') #get service name running on port
 				#banner = s.send('howareyou\r\n')
 				#bannerrecv = s.recv(100)
 			except:
 				continue
 			if result == 0:
 					print("Port {} is open".format(port))
-					print("Service running on the port is " + service)
+					print("Service running on the port is " + service) 
 					#print(port)
 					#print(bannerrecv)
 
